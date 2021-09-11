2e98eb62606168f0108595e3e2ef977bd108ad21
Rustam <lrz93@mail.ru>
2021-09-11 17:48:36 +0300

    up 2

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const myData = data;
+const personData = data,
+    tableBody = document.querySelector('#tableData');
 
-const table = {
-    fName: document.querySelector('#first_name'),
-    lName: document.querySelector('#last_name'),
-    about: document.querySelector('#about'),
-    eye: document.querySelector('#eye_color'),
+function loadTable(person) {
+    tableBody.innerHTML += `
+    <tr>
+    <td>${person.name.firstName}</td>
+    <td>${person.name.lastName}</td>
+    <td class="about">${person.about}</td>
+    <td>${person.eyeColor}</td>
+    </tr>`;
 }
 
-
-function appendEl(col, word) {
-    let div = document.createElement('div');
-    div.innerText = word;
-    col.append(div);
-}
-
-
-
-myData.forEach(el => {
-    appendEl(table.fName, el.name.firstName);
-    appendEl(table.lName, el.name.lastName);
-    appendEl(table.about, el.about);
-    appendEl(table.eye, el.eyeColor);
+personData.forEach(person => {
+    loadTable(person);
 });
+
+function sortTableMin() {
+    return personData.sort((prev, next) => {
+        if (prev.name.firstName > next.name.firstName) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+}
+
+console.log(sortTableMin());
