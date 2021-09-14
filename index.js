5258d11171fd47001260fa30d59cea2ae87878de
Rustam <lrz93@mail.ru>
2021-09-14 13:41:05 +0300

    up 3

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const personData = data,
+const personsData = data,
     tableBody = document.querySelector('#tableData'),
     dataEdit = document.querySelector('#data_edit'),
     butCloseDE = document.querySelector('#but_close'),
     butSendDE = document.querySelector('#but_send');
 
+let idPersonData = '';
+
 let lastSort = '';
 
 function loadTable(person) {
+
     tableBody.innerHTML += `
     <tr id=${person.id}>
     <td>${person.name.firstName}</td>
     <td>${person.name.lastName}</td>
     <td class="about">${person.about}</td>
-    <td>${person.eyeColor}</td>
+    <td><div style=" display: flex; height: auto; width: auto; justify-content: space-evenly;"> ${person.eyeColor} <div style=" background-color: ${person.eyeColor}; width: 15px; height: 15px;"></div></div></td>
     </tr>`;
 }
 
 function createTable() {
-    personData.forEach(person => {
+
+    tableBody.innerHTML = '';
+
+    personsData.forEach(person => {
         loadTable(person);
     });
 }
 
 function sortController(pI, pIG) {
+
     if (lastSort == 'down') {
         sortTableToUp(pI, pIG);
         lastSort = 'up';
@@ -33,7 +40,8 @@ function sortController(pI, pIG) {
 }
 
 function sortTableToDown(personInfo, personInfoGroup) {
-    return personData.sort((prev, next) => {
+
+    return personsData.sort((prev, next) => {
         if (personInfoGroup) {
             if (prev[personInfoGroup][personInfo] > next[personInfoGroup][personInfo]) {
                 return 1;
@@ -51,7 +59,8 @@ function sortTableToDown(personInfo, personInfoGroup) {
 }
 
 function sortTableToUp(personInfo, personInfoGroup) {
-    return personData.sort((prev, next) => {
+
+    return personsData.sort((prev, next) => {
         if (personInfoGroup) {
             return prev[personInfoGroup][personInfo] > next[personInfoGroup][personInfo] ? -1 : 1;
         } else {
@@ -60,39 +69,61 @@ function sortTableToUp(personInfo, personInfoGroup) {
     });
 }
 
-function formDataEdit(id) {
-    console.log(id);
+function formDataOpen() {
+
+    let personData = personsData.find(el => el.id == idPersonData);
+
+    document.getElementById('data_firstName').value = personData.name.firstName;
+    document.getElementById('data_lastName').value = personData.name.lastName;
+    document.getElementById('data_about').value = personData.about;
+    document.getElementById('data_eyeColor').value = personData.eyeColor;
+    document.getElementById('data_phone').value = personData.phone;
 }
 
+function formDataEdit() {
+
+    let personData = personsData.find(el => el.id == idPersonData);
+
+    personData.name.firstName = document.getElementById('data_firstName').value;
+    personData.name.lastName = document.getElementById('data_lastName').value;
+    personData.about = document.getElementById('data_about').value;
+    personData.eyeColor = document.getElementById('data_eyeColor').value;
+    personData.phone = document.getElementById('data_phone').value;
+
+    createTable();
+}
 
 document.addEventListener('DOMContentLoaded', createTable);
 
 document.addEventListener('click', function (e) {
+
     switch (e.target.id) {
         case 'firstName': sortController('firstName', 'name');
-            tableBody.innerHTML = '';
             createTable();
             break;
         case 'lastName': sortController('lastName', 'name');
-            tableBody.innerHTML = '';
             createTable();
             break;
         case 'about': sortController('about');
-            tableBody.innerHTML = '';
             createTable();
             break;
         case 'eyeColor': sortController('eyeColor');
-            tableBody.innerHTML = '';
             createTable();
             break;
     }
 
     if (e.target == document.body || e.target == document.html) {
         data_edit.style.display = 'block';
-        
+        formDataOpen();
     } else if (e.target == butCloseDE) {
         data_edit.style.display = 'none';
+
+    } else if (e.target == butSendDE) {
+        formDataEdit();
     } else {
-        formDataEdit(e.target);
+        if (e.target.tagName == 'TD') {
+            idPersonData = e.target.parentNode.id;
+            formDataOpen();
+        }
     }
 });
