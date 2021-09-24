8134fa5255a7b60a9b4c44641c7367b33b2604bf
Rustam <lrz93@mail.ru>
2021-09-24 14:02:55 +0300

    v 1.01f+c

--- index.js
+++ index.js
@@ -32,74 +32,72 @@ const id_column = document.querySelector('#id_column'),
     head_table = document.querySelector('#head_table'),
     sort_list = document.querySelector('#sort_list');
 
-function checkColumnHead() {
+
+document.addEventListener('DOMContentLoaded', createTable()); // Загружаем таблицу
+
+function createTable(fRow = firstRow, lRow = lastRow) { // Функция создает таблицу в DIV с id=tableBody. 
+
+    tableBody.innerHTML = ''; // очищаем содержимое элемента
+
+    loadHeadTable(); // загружает заголовки столбцов таблицы
+
+    createDataAfterSort(); // создаем данные
+
+    while (fRow < lRow && fRow < personsDataAfterSort.length) { // выводим данные в количестве в соответствии с выбранным способом вывода данных
+        loadBodyTable(personsDataAfterSort[fRow]); // загружаем в DOM очередной элемент из массива
+        fRow++; // 
+    }
+
+    createPageList(); // т.к. после сортировки или изменения данных кол-во элементов может измениться необходимо пересчитать сколько страниц данных будет в выбранном способе вывода данных, а так же на какой странице находится таблица
+}
+
+function loadHeadTable() { // загружаем заголовки столбцов таблицы
+    head_table.innerHTML = `                
+    <tr>
+        ${checkColumnHead()}
+    </tr>`; // создает шапку с заголовоками и проверяем есть ли он в списке тех заголовков которые должны отобразиться
+}
+
+function checkColumnHead() { // проверяем из списка какие заголовки должны отобразиться и возвращаем их
     let ans = '';
-    if (id_column.checked) {
+    if (id_column.checked) { // заголовок id
         ans += `<th id="id" class="id column_name">id</th>`;
     }
-    if (firstName_column.checked) {
+    if (firstName_column.checked) { // заголовок firstName
         ans += `<th id="firstName" class="other column_name">Имя (firstName)</th>`;
     }
-    if (lastName_column.checked) {
+    if (lastName_column.checked) { // заголовок lastName
         ans += ` <th id="lastName" class="other column_name">Фамилия (lastName)</th>`;
     }
-    if (about_column.checked) {
+    if (about_column.checked) { // заголовок about
         ans += `<th id="about" class="column_name">Описание (about)</th>`;
     }
-    if (eyeColor_column.checked) {
+    if (eyeColor_column.checked) { // заголовок eyeColor
         ans += `<th id="eyeColor" class="other column_name">Цвет глаз (eyeColor)</th>`;
     }
-    if (phone_column.checked) {
+    if (phone_column.checked) { // заголовок phone
         ans += `<th id="phone" class="other column_name">Телефон (phone)</th>`;
     }
     return ans;
 }
 
-function loadHeadTable() {
-    head_table.innerHTML = `                
-    <tr>
-        ${checkColumnHead()}
-    </tr>`;
+function createDataAfterSort() { // создаем данные после сортировки
+    personsDataAfterSort = []; 
+
+    for (person of personsData) { // перебераем элементы в массиве personsData
+        if (check(person)) { // и проверяем фильтровались ли они по значению
+            personsDataAfterSort.push(person); // добавляем в массив элемент
+        }
+    }
 }
 
-function checkColumnBody(person) {
-    let ans = '';
-    if (id_column.checked) {
-        ans += `<td class="id">${person.id}</td>`;
-    }
-    if (firstName_column.checked) {
-        ans += `<td class="other">${person.name.firstName}</td>`;
-    }
-    if (lastName_column.checked) {
-        ans += `<td class="other">${person.name.lastName}</td>`;
-    }
-    if (about_column.checked) {
-        ans += `<td class="about">${person.about}</td>`;
-    }
-    if (eyeColor_column.checked) {
-        ans += `<td class="other"><div class="eyeColor">${person.eyeColor}<div style="background-color: ${person.eyeColor}"></div></div></td>`;
-    }
-    if (phone_column.checked) {
-        ans += `<td class="other">${person.phone}</td>`;
-    }
-    return ans;
-}
-
-function loadBodyTable(person) {
-
-    tableBody.innerHTML += `
-    <tr id=${person.id}>
-    ${checkColumnBody(person)}
-    </tr>`;
-}
-
-function check(person) {
-    if (sort_data.id) {
-        if (sort_data.id.indexOf(person.id) == -1) {
+function check(person) { // проверяем какие заголовки и значение были выбраны
+    if (sort_data.id) { // если таблица сортировалась по столбку id
+        if (sort_data.id.indexOf(person.id) == -1) { // в данном столбце выбраного значение нет
             return false;
         }
     }
-    if (sort_data.firstName) {
+    if (sort_data.firstName) { // если таблица сортировалась по столбку firstName и т.д.
         if (sort_data.firstName.indexOf(person.name.firstName) == -1) {
             return false;
         }
@@ -124,115 +122,99 @@ function check(person) {
             return false;
         }
     }
-    return true;
+    return true; // если по столбцам сортировак не было или значение было найдено
 }
 
-function createDataAfterSort() {
-    personsDataAfterSort = [];
+function pageShowData() { // функция определяет 
 
-    for (person of personsData) {
-        if (check(person)) {
-            personsDataAfterSort.push(person);
-        }
+    lastRow = maxRow * pageCount;
+    firstRow = lastRow - maxRow;
+
+    createTable();
+}
+
+function loadBodyTable(person) { // добавляем очередной элемент в тело таблицы
+
+    tableBody.innerHTML += `
+    <tr id=${person.id}>
+    ${checkColumnBody(person)}
+    </tr>`; // создаем строку и проверяем какие данные должны отобразиться
+}
+
+function checkColumnBody(person) { // проверяем столбцы и создаем элементы строки
+    let ans = '';
+    if (id_column.checked) {
+        ans += `<td class="id">${person.id}</td>`;
     }
-}
-
-function createTable(fRow = firstRow, lRow = lastRow) {
-
-    tableBody.innerHTML = '';
-
-    loadHeadTable();
-
-    createDataAfterSort();
-
-    while (fRow < lRow && fRow < personsDataAfterSort.length) {
-        loadBodyTable(personsDataAfterSort[fRow]);
-        fRow++;
+    if (firstName_column.checked) {
+        ans += `<td class="other">${person.name.firstName}</td>`;
     }
-
-    createPageList();
-
-    console.log(fRow, fRow);
+    if (lastName_column.checked) {
+        ans += `<td class="other">${person.name.lastName}</td>`;
+    }
+    if (about_column.checked) {
+        ans += `<td class="about">${person.about}</td>`;
+    }
+    if (eyeColor_column.checked) {
+        ans += `<td class="other"><div class="eyeColor">${person.eyeColor}<div style="background-color: ${person.eyeColor}"></div></div></td>`; // в этом элементе создаем DIV у которого в стилях присваеваем значение цвета
+    }
+    if (phone_column.checked) {
+        ans += `<td class="other">${person.phone}</td>`;
+    }
+    return ans;
 }
 
-function createPageList() {
+function createPageList() { // рассчитаем сколько страниц выбранном способе вывода данных будет и на какой страницы находится таблица
 
-    if (personsDataAfterSort.length / maxRow > Math.floor(personsDataAfterSort.length / maxRow)) {
+    if (personsDataAfterSort.length / maxRow > Math.floor(personsDataAfterSort.length / maxRow)) { // определяем будет ли остаток
         pagesMaxCount = Math.floor(personsDataAfterSort.length / maxRow) + 1;
     } else {
         pagesMaxCount = personsDataAfterSort.length / maxRow;
     }
 
-    page.innerHTML = '';
-
-    if (maxRow < personsDataAfterSort.length) {
-        pages.style.display = 'flex';
-        page.innerHTML += `${pageCount} of ${pagesMaxCount}`;
+    if (maxRow < personsDataAfterSort.length) { // если способ вывода данных меньше длины таблицы
+        pages.style.display = 'flex'; // отображаем интерфей перемежения по страницам
+        page.innerHTML = `${pageCount} of ${pagesMaxCount}`; // отображает на какой страницы из общего кол-ва страниц
     } else {
-        pages.style.display = 'none';
+        pages.style.display = 'none'; // в противном случае скрывает интерфейс т.к. взаимодействие с ним бессмысленна
     }
 }
 
-function pageShowData() {
-
-    console.log(pageCount);
-
-        lastRow = maxRow * pageCount;
-        firstRow = lastRow - maxRow;
-
-    createTable();
+function modeShowData(value = maxRow) { // устанавливаем параметры отображения данных
+    firstRow = 0; // индекс первого эдемента
+    pageCount = 1; // номер страцы
+    maxRow = value; // максимальное кол-во элементов на странице
+    lastRow = maxRow; // последний элемент страцы
+    createTable(); // пересоздаем таблицу
 }
 
-function modeShowData(value = maxRow) {
-    firstRow = 0;
-    pageCount = 1;
-    maxRow = value;
-    lastRow = maxRow;
-    createTable();
-}
-
-function openSortWindow(param, addit_param = '') {
+function openSortWindow(param, addit_param = '') { // открывает окно для сортировки столбца. Функция принимает 2 значения: ключ и дополнительный ключ
 
     person_param = param,
         addit_person_param = addit_param;
 
-    sortTarget();
+    sortTarget(); // создаем список значений
 
     sort_table.style.display = 'block';
 }
 
-function sortTargetTable() {
+function sortTargetTable() { // создаем массив сортированных данных по значению
 
-    sort_data[person_param] = [];
-    for (child of sort_list.childNodes) {
-        if (child.tagName == "DIV" && child.childNodes[1].checked) {
-            sort_data[person_param].push(child.childNodes[1].id);
+    sort_data[person_param] = []; // по ключу столбца создаем массив 
+    for (child of sort_list.childNodes) { // перебираем список элементов
+        if (child.tagName == "DIV" && child.childNodes[1].checked) { // находим checkbox и проверяем постален ли в нем флажек
+            sort_data[person_param].push(child.childNodes[1].id); // добавляем id элемента в массив. id идентичен значению 
         }
     }
-    firstRow = 0;
-    if (maxRow > personsDataAfterSort.length) {
-        lastRow = personsDataAfterSort.length;
-    }
 
-    modeShowData();
+    modeShowData(); 
 }
 
-function createSortTarget(element) {
-
-    if (list_for_sort.indexOf(element) == -1) {
-
-        list_for_sort.push(element);
-        sort_list.innerHTML += `
-            <div>${element} <input id="${element}" type="checkbox"></div>
-        `;
-    }
-}
-
-function sortTarget() {
+function sortTarget() { // список значений для сортировки
 
     list_for_sort = [];
     sort_list.innerHTML = '';
-    if (addit_person_param) {
+    if (addit_person_param) { // если доп. ключ есть
         personsData.forEach(element => {
             createSortTarget(element[addit_person_param][person_param]);
         });
@@ -243,7 +225,17 @@ function sortTarget() {
     }
 }
 
-function selectAll() {
+function createSortTarget(element) { // создаем список значений
+
+    if (list_for_sort.indexOf(element) == -1) { // что бы значения в списке не повторялись, создаем массив значений
+        list_for_sort.push(element); 
+        sort_list.innerHTML += `
+            <div>${element} <input id="${element}" type="checkbox"></div>
+        `;
+    }
+}
+
+function selectAll() { // выбор всех значений
     for (child of sort_list.childNodes) {
         if (child.tagName == "DIV" && !child.childNodes[1].checked) {
             child.childNodes[1].checked = true;
@@ -251,7 +243,7 @@ function selectAll() {
     }
 }
 
-function clearAll() {
+function clearAll() { // убрать флажки
     for (child of sort_list.childNodes) {
         if (child.tagName == "DIV" && child.childNodes[1].checked) {
             child.childNodes[1].checked = false;
@@ -259,7 +251,7 @@ function clearAll() {
     }
 }
 
-function sortTableToDown() {
+function sortTableToDown() { // сортировать по нисходящей а-я
 
     personsData.sort((prev, next) => {
         if (addit_person_param) {
@@ -280,7 +272,7 @@ function sortTableToDown() {
     createTable();
 }
 
-function sortTableToUp() {
+function sortTableToUp() { // сортировать по восходящей я-а
 
     personsData.sort((prev, next) => {
         if (addit_person_param) {
@@ -293,9 +285,9 @@ function sortTableToUp() {
     createTable();
 }
 
-function formDataOpen() {
+function formDataOpen() { // загружаем форму редактирования данных
 
-    if (idPersonData) {
+    if (idPersonData) { // если ранее строка таблицы была выбрано т.е. по ней был клик то в поле input выводяться значения строки
         let personData = personsData.find(el => el.id == idPersonData);
 
         document.getElementById('data_firstName').value = personData.name.firstName;
@@ -306,11 +298,11 @@ function formDataOpen() {
 
         butSendDE.disabled = false;
     } else {
-        butSendDE.disabled = true;
+        butSendDE.disabled = true; // если строка выбрана кнопка "сохранить" блокируется
     }
 }
 
-function formDataEdit() {
+function formDataEdit() { // сохраняет данные
 
     let personData = personsData.find(el => el.id == idPersonData);
 
@@ -320,12 +312,10 @@ function formDataEdit() {
     personData.eyeColor = document.getElementById('data_eyeColor').value;
     personData.phone = document.getElementById('data_phone').value;
 
-    createTable();
+    createTable(); // создаем таблицу с обновленными данными
 }
 
-document.addEventListener('DOMContentLoaded', pageShowData());
-
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function (e) { // отслеживаем клик мыши
 
     switch (e.target.id) {
         case 'firstName': openSortWindow('firstName', 'name');
@@ -354,8 +344,9 @@ document.addEventListener('click', function (e) {
             break;
         case 'clear_all': clearAll();
             break;
-
-        case 'show_10_row': modeShowData(10);
+        
+            // способы отображения данных таблицы
+        case 'show_10_row': modeShowData(10); 
             break;
         case 'show_25_row': modeShowData(25);
             break;
@@ -364,20 +355,20 @@ document.addEventListener('click', function (e) {
         case 'show_all_row': modeShowData(personsDataAfterSort.length);
             break;
 
-        case 'but_close': data_edit.style.display = 'none';
+        case 'but_close': data_edit.style.display = 'none'; // при клике на кнопку "закрыть" форма редактирования данных будет скрыта
             break;
-        case 'but_send': formDataEdit();
+        case 'but_send': formDataEdit(); // при клике на кнопку "сохранить" в форме редактирования данных будут сохранены в таблицу
             break;
-        case 'show_hide': createTable();
+        case 'show_hide': createTable(); // перезагружаем таблицу для того, что бы отобразить выбранные столбцы
             break;
 
-        case 'arrow_down':
+        case 'arrow_down': // стрелка возврата на предыдущую страницу
             if (pageCount - 1 > 0) {
                 pageCount--;
                 pageShowData();
             }
             break;
-        case 'arrow_up':
+        case 'arrow_up': // стрелка перехода на следующую страницу
             if (pageCount + 1 <= pagesMaxCount) {
                 pageCount++;
                 pageShowData();
@@ -385,13 +376,13 @@ document.addEventListener('click', function (e) {
             break;
     }
 
-    if (e.target == document.body) {
+    if (e.target == document.body) { // при клике на body отображается форма редактирования данных
         data_edit.style.display = 'block';
-        formDataOpen();
+        formDataOpen(); // загружаем форму
     }
 
-    if (e.target.tagName == 'TD') {
+    if (e.target.tagName == 'TD') { // при клике на строку данные загружаются, но форма редактирования данных не отображается
         idPersonData = e.target.parentNode.id;
-        formDataOpen();
+        formDataOpen(); // загружаем форму
     }
 });
